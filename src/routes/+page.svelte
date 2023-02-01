5f7a72bc6b35ddf8fb5095c02adc7d2603a72479
tglide <26071571+TGlide@users.noreply.github.com>
2023-02-01 17:37:19 +0000

    :sparkles: add scss variable test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,31 @@
 <div class="container">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+	<div class="u-grid u-gap-16">
+		<div class="box">
+			<p>Theme I'm in:</p>
+		</div>
+		<div class="theme-dark">
+			<div class="box">
+				<p>Theme I'm in:</p>
+			</div>
+		</div>
+	</div>
 </div>
+
+<style lang="scss">
+	$theme-dark: '.theme-dark';
+
+	.box {
+		--content: ' light';
+
+		#{$theme-dark} & {
+			--content: ' dark';
+		}
+
+		p::after {
+			content: var(--content);
+		}
+	}
+</style>
